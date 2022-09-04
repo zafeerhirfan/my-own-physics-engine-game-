fd72fd4085b7b1c37da428769ae77b273d98e9e4
zafeerhirfan <94226398+zafeerhirfan@users.noreply.github.com>
2022-09-04 09:33:24 +0530

    Add files via upload

---
+++ my own physics engine game/Pro34-Project-Boilerplate-main/sketch.js
@@ -0,0 +1,25 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+
+
+
+function setup() {
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+  
+}
+
+
+function draw() 
+{
+  background(51);
+  Engine.update(engine);
+  
+}
+
